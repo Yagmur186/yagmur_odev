b6f5258d600f21ee27cee1980a804ff1672525a7
Yagmur186 <150551816+Yagmur186@users.noreply.github.com>
2023-12-04 18:40:29 +0300

    Ödev Hafta 8.cpp

---
+++ Ödev Hafta 8.cpp
@@ -0,0 +1,69 @@
+KODLAYICI1
+#include<iostream>
+using namespace std;
+
+int karesi_yarısı(int sayi1, int sayi2)
+{
+cout << "Sayiyi girin: ";
+cin >> sayi1;
+if ( sayi1 % 2 == 0)
+cout << sayi1 << " tektir.";
+int sayi1 kare = sayi*sayi
+for (int i=0; i<5; i++)
+cout << "Sayiyi girin: ";
+cin >> sayi2;
+if ( sayi2 % 2 == 0)
+cout << sayi2<< " çifttir.";
+int sayi2= sayi2/2
+
+
+return rozetler;
+{
+
+}
+int main()
+  int sayi1 kare = sayi*sayi
+  int sayi2= sayi2/2
+}
+    
+ 
+
+KODLAYICI 2
+#include<iostream>
+using namespace std;
+void iki_ekle(int y +int 2tane)
+{
+    for(int i=0; i< 2tane; i++ )
+        cout<< y;
+}
+int main()
+{
+    iki_ekle(4,6);
+}
+KODLAYICI 3
+#include<iostream>
+using namespace std;
+
+int rozet_yaz(int ‘Ayşe’ rozetler; int dizi[5])
+{
+    int sayi=0;
+for (int i=0; i<5; i++)
+}
+   if(sayi<dizi[i])
+    sayi=dizi[i];
+
+
+      return rozetler;
+{
+
+}
+
+int main()
+{
+
+ int rozetler []={1,9,3,5,6};
+ int rozetler=toplama(rozetler);
+ int ogrenci ismi []={"Ayşe"};
+ int ogrenci ismi=toplama(ogrenci ismi);
+ cout<<rozetler+ogrenci ismi;
+}
