18d71265d97ea7293e4f79184dab3e2cc1d13d3d
Yagmur186 <150551816+Yagmur186@users.noreply.github.com>
2023-12-10 17:52:53 +0300

    Ödev Hafta 9.cpp

---
+++ Ödev Hafta 9.cpp
@@ -0,0 +1,41 @@
+DENETLEYİCİ:
+#include <iostream
+using namespace std;
+class Tugla
+private:
+    float uzunluk, yukseklik;
+public:
+    void deger_atama(float);
+    float 50 tugla ()
+    {
+        return uzunluk* yukseklik;
+    }
+    };
+    void Tugla:: deger_atama(float a)
+    {
+       metrekare= a;
+    }
+    int main
+    Tugla tugla; tugla.deger_atama(5.8);
+    cout <<"Tugla sayisi:"<<tugla.sayisi()<<"\n";
+    retun 0;
+    KODLAYICI:
+    #include <iostream
+using namespace std;
+class Silindir
+
+public:
+    float yar_cap, yukseklik;
+    float su_tuketimini_bul(float yari_cap, yukseklik)
+    {
+        return 3.12* yari_cap* yukseklik;
+    }
+
+
+    int main
+    Silindir silindir;
+    cout<<"Silindirin yaricapini ve yuksekliğini giriniz"<< endl;
+    cin>> silindir. yari_cap.yukseklik;
+    cout <<"Sutuketimi:"<<silindir.su_tuketimini_bul (silindir.yari_cap.yukseklik)<<"\n";
+    retun 0;
+
